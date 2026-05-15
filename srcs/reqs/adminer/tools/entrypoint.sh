2ce4fe68e4159665db7ddb3bc264eb58c08b3411
lareii <lareii@proton.me>
2026-05-15 15:05:57 +0300

    refactor: remove redundant entrypoint scripts

--- srcs/reqs/adminer/tools/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# -S: builtin web server
-# -t: document root, target directory
-exec php -S 0.0.0.0:8080 -t /var/www/html
--- srcs/reqs/redis/tools/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# allkeys-lru: delete first the least recently used keys when maxmemory limit is reached
-exec redis-server --bind 0.0.0.0 --daemonize no --protected-mode no \
-     --maxmemory 256mb --maxmemory-policy allkeys-lru

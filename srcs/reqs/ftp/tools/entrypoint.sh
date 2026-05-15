c575baa92aaac3b8cc2e5208e0957a052a6c9ed3
lareii <lareii@proton.me>
2026-05-15 12:12:22 +0300

    refactor: move ftp config from entrypoint script to new file

--- srcs/reqs/ftp/tools/entrypoint.sh
+++ srcs/reqs/ftp/tools/entrypoint.sh
@@ -9,24 +9,4 @@ mkdir -p /var/run/vsftpd/empty
 id -u ftpuser &>/dev/null || useradd -d /var/www/wordpress -M ftpuser
 printf 'ftpuser:%s\n' "$FTP_PASSWORD" | chpasswd
 
-cat << EOF > /etc/vsftpd.conf
-listen=YES
-
-anonymous_enable=NO
-local_enable=YES
-write_enable=YES
-
-local_root=/var/www/wordpress
-chroot_local_user=YES
-allow_writeable_chroot=YES
-
-pasv_enable=YES
-pasv_min_port=21000
-pasv_max_port=21100
-
-# disable seccomp sandbox to avoid docker related issues 
-seccomp_sandbox=NO
-background=NO
-EOF
-
 exec vsftpd /etc/vsftpd.conf
